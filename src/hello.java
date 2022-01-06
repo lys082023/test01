f8afbe7fb7d7bc9c75548a500472ab6e38e3b8d2
liyuns <443005904@qq.com>
2022-01-06 11:21:32 +0800

    test2

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,12 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] temp = {30,543,1,4,6,300,40};
+
+        BubbleSort sort = new BubbleSort();
+        sort.bubbleSort(temp);
+
+        for (int i=0;i<temp.length;i++){
+            System.out.println(temp[i]);
+        }
     }
 }
